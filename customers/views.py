8aac162b7b1cd15b430ce9f6bef94ff9fae299ba
Imtiaz Sajid <96991963+IzSajid@users.noreply.github.com>
2023-10-25 01:03:50 +0600

    Before trying anything with post

--- customers/views.py
+++ customers/views.py
@@ -1,16 +1,31 @@
 from customers.models import Customer
-from django.http import JsonResponse,Http404
+from django.http import JsonResponse
 from customers.serializer import CustomerSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+#customers list
+@api_view(['GET', 'POST'])
 def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
-    return JsonResponse({'customers': serializer.data})
+    return Response({'customers': serializer.data})
 
+#customer section
+@api_view(['GET', 'POST', 'DELETE'])
 def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
-        raise Http404('Customer not found')
-    serializer = CustomerSerializer(data, many=False)
-    return JsonResponse({'customer': serializer.data})
+        return Response({'message': 'The customer does not exist'}, status=status.HTTP_404_NOT_FOUND)
     
+    if request.method == 'GET':
+        serializer = CustomerSerializer(data, many=False)
+        return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
+    
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response({'message': 'Customer was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    
+   
