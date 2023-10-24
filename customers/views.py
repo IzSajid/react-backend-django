fb4e23c929e31aae4d3da3060f8ba832edc3804a
Imtiaz Sajid <96991963+IzSajid@users.noreply.github.com>
2023-10-25 01:41:24 +0600

    CRUD customer completed

--- customers/views.py
+++ customers/views.py
@@ -1,31 +1,40 @@
 from customers.models import Customer
-from django.http import JsonResponse
-from customers.serializer import CustomerSerializer
+from django.http import JsonResponse, Http404
+from customers.serializers import CustomerSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-#customers list
 @api_view(['GET', 'POST'])
 def customers(request):
-    data = Customer.objects.all()
-    serializer = CustomerSerializer(data, many=True)
-    return Response({'customers': serializer.data})
+    if request.method == 'GET':
+        data = Customer.objects.all()
+        serializer = CustomerSerializer(data, many=True)
+        return Response({'customers': serializer.data})
+    
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer' : serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#customer section
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'POST', 'DELETE'])    
 def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
-        return Response({'message': 'The customer does not exist'}, status=status.HTTP_404_NOT_FOUND)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
-        serializer = CustomerSerializer(data, many=False)
-        return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
-    
+        serializer = CustomerSerializer(data)
+        return Response({'customer': serializer.data})
     elif request.method == 'DELETE':
         data.delete()
-        return Response({'message': 'Customer was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
-    
-   
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
